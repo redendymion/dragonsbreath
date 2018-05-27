9e8195d50cca6b2ca4dcbbc29439cb43165e58a6
redendymion <eaelectronics@yahoo.ie>
2018-05-27 16:06:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<script>
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+});
+</script>
